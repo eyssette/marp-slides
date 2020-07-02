911b12c87a0e9e00f90367a4c9e438a2a176868e
eyssette <cedric.eyssette@gmail.com>
2020-07-03 00:27:12 +0200

    update

--- slides/generate-index.py
+++ slides/generate-index.py
@@ -29,7 +29,9 @@ def index_folder(folderPath):
 	for file in files:
 		#Avoiding index.html files
 		if file != 'index.html':
-			indexText += "\t\t<li>\n\t\t\t<a href='" + file + "'>" + file + "</a>\n\t\t</li>\n"
+			exthtml = file[-4:]
+			if exthtml == 'html': 
+				indexText += "\t\t<li>\n\t\t\t<a href='" + file + "'>" + file + "</a>\n\t\t</li>\n"
 		#Recursive call to continue indexing
 		if os.path.isdir(folderPath+'/'+file):
 			index_folder(folderPath + '/' + file)
